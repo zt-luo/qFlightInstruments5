547f371c80e8c8dd7e43c7379f62a29c183245c4
ztluo <me@ztluo.dev>
2019-05-13 10:25:06 +0800

    use Modern C++

--- qFlightInstruments.cpp
+++ qFlightInstruments.cpp
@@ -47,7 +47,7 @@ void QADI::canvasReplot_slot(void)
 }
 
 
-void QADI::resizeEvent(QResizeEvent *event)
+void QADI::resizeEvent(QResizeEvent *)
 {
     m_size = qMin(width(),height()) - 2*m_offset;
 }
@@ -80,25 +80,25 @@ void QADI::paintEvent(QPaintEvent *)
     {
         int y_min, y_max;
 
-        y_min = m_size/2*-40.0/45.0;
-        y_max = m_size/2* 40.0/45.0;
+        y_min = static_cast<int>(m_size/2*-40.0/45.0);
+        y_max = static_cast<int>(m_size/2* 40.0/45.0);
 
-        int y = m_size/2*pitch_tem/45.;
+        int y = static_cast<int>(m_size/2*pitch_tem/45.0);
         if( y < y_min ) y = y_min;
         if( y > y_max ) y = y_max;
 
-        int x = sqrt(m_size*m_size/4 - y*y);
+        int x = static_cast<int>(sqrt(m_size*m_size/4 - y*y));
         qreal gr = atan((double)(y)/x);
         gr = gr * 180./3.1415926;
 
         painter.setPen(blackPen);
         painter.setBrush(bgSky);
         painter.drawChord(-m_size/2, -m_size/2, m_size, m_size,
-                          gr*16, (180-2*gr)*16);
+                          static_cast<int>(gr*16), static_cast<int>((180-2*gr)*16));
 
         painter.setBrush(bgGround);
         painter.drawChord(-m_size/2, -m_size/2, m_size, m_size,
-                          gr*16, -(180+2*gr)*16);
+                          static_cast<int>(gr*16), static_cast<int>(-(180+2*gr)*16));
     }
 
     // set mask
@@ -133,12 +133,12 @@ void QADI::paintEvent(QPaintEvent *)
 
             if( i == 0 ) {
                 painter.setPen(pitchZero);
-                l = l * 1.8;
+                l = static_cast<int>(l * 1.8);
             } else {
                 painter.setPen(pitchPen);
             }
 
-            y = m_size/2*p/45.0 - m_size/2*pitch_tem/45.;
+            y = static_cast<int>(m_size/2*p/45.0 - m_size/2*pitch_tem/45.0);
             x = l;
 
             r = sqrt(x*x + y*y);
@@ -160,7 +160,7 @@ void QADI::paintEvent(QPaintEvent *)
 
         // draw marker
         int     markerSize = m_size/20;
-        float   fx1, fy1, fx2, fy2, fx3, fy3;
+        qreal   fx1, fy1, fx2, fy2, fx3, fy3;
 
         painter.setBrush(QBrush(Qt::red));
         painter.setPen(Qt::NoPen);
@@ -190,7 +190,7 @@ void QADI::paintEvent(QPaintEvent *)
     // draw roll degree lines
     {
         int     nRollLines = 36;
-        float   rotAng = 360.0 / nRollLines;
+        qreal   rotAng = 360.0 / nRollLines;
         int     rollLineLeng = m_size/25;
         double  fx1, fy1, fx2, fy2;
         int     fontSize = 8;
@@ -312,7 +312,7 @@ void QCompass::canvasReplot_slot(void)
     update();
 }
 
-void QCompass::resizeEvent(QResizeEvent *event)
+void QCompass::resizeEvent(QResizeEvent *)
 {
     m_size = qMin(width(),height()) - 2*m_offset;
 }
@@ -353,7 +353,7 @@ void QCompass::paintEvent(QPaintEvent *)
     // draw yaw lines
     {
         int     nyawLines = 36;
-        float   rotAng = 360.0 / nyawLines;
+        qreal   rotAng = 360.0 / nyawLines;
         int     yawLineLeng = m_size/25;
         double  fx1, fy1, fx2, fy2;
         int     fontSize = 8;
@@ -368,22 +368,22 @@ void QCompass::paintEvent(QPaintEvent *)
                 s = "N";
                 painter.setPen(bluePen);
 
-                painter.setFont(QFont("Arial", fontSize*1.3));
+                painter.setFont(QFont("Arial", static_cast<int>(fontSize*1.2)));
             } else if ( i == 9 ) {
                 s = "W";
                 painter.setPen(blackPen);
 
-                painter.setFont(QFont("Arial", fontSize*1.3));
+                painter.setFont(QFont("Arial", static_cast<int>(fontSize*1.2)));
             } else if ( i == 18 ) {
                 s = "S";
                 painter.setPen(redPen);
 
-                painter.setFont(QFont("Arial", fontSize*1.3));
+                painter.setFont(QFont("Arial", static_cast<int>(fontSize*1.2)));
             } else if ( i == 27 ) {
                 s = "E";
                 painter.setPen(blackPen);
 
-                painter.setFont(QFont("Arial", fontSize*1.3));
+                painter.setFont(QFont("Arial", static_cast<int>(fontSize*1.2)));
             } else {
                 s = QString("%1").arg(i*rotAng);
                 painter.setPen(blackPen);
@@ -594,7 +594,7 @@ void QKeyValueListView::listUpdate_slot(void)
 
     for(i=0, it=m_data.begin(); it!=m_data.end(); i++, it++) {
         // set name cell
-        if( this->item(i, 0) != NULL ) {
+        if( this->item(i, 0) != nullptr ) {
             this->item(i, 0)->setText(it.key());
         } else {
             QTableWidgetItem* item = new QTableWidgetItem();
@@ -610,7 +610,7 @@ void QKeyValueListView::listUpdate_slot(void)
         }
 
         // set value cell
-        if( this->item(i, 1) != NULL ) {
+        if( this->item(i, 1) != nullptr ) {
             this->item(i, 1)->setText(it.value());
         } else {
             QTableWidgetItem* item = new QTableWidgetItem();
